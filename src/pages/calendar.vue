77af66a74ffff97caebca4c55f9873b1050bedf5
yuki <yk.yuki21@gmail.com>
2019-05-11 23:03:21 +0900

    Remove vue-property-decorator from Calendar

--- src/pages/calendar.vue
+++ src/pages/calendar.vue
@@ -90,21 +90,40 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
 import Event from '~/lib/Event.ts'
 import { EventType } from '~/lib/EventType.ts'
 import axios from 'axios'
 
-@Component
-export default class Calendar extends Vue {
-  today = new Date()
-  days = ['日', '月', '火', '水', '木', '金', '土']
-  year = this.today.getFullYear()
-  month = this.today.getMonth() + 1
-  weekList: object[] = []
-  eventList?: Event[] = undefined
-  isLoading: boolean = true
+interface IData {
+  today: Date
+  days: string[]
+  year: number
+  month: number
+  weekList: object[]
+  eventList: Event[] | undefined
+  isLoading: boolean
+}
 
+const today = new Date()
+
+export default Vue.extend({
+  data(): IData {
+    return {
+      today: today,
+      days: ['日', '月', '火', '水', '木', '金', '土'],
+      year: today.getFullYear(),
+      month: today.getMonth() + 1,
+      weekList: [],
+      eventList: undefined,
+      isLoading: true
+    }
+  },
+  watch: {
+    eventList: function() {
+      this.weekList = this.CreateWeekList(this.year, this.month, this.eventList)
+    }
+  },
   created() {
     this.GetEventList().then(result => {
       if (result !== undefined) {
@@ -113,108 +132,121 @@ export default class Calendar extends Vue {
     })
     this.weekList = this.CreateWeekList(this.year, this.month, this.eventList)
     this.isLoading = false
-  }
-
-  private CreateWeekList(targetYear: number, targetMonth: number, eventList?: Event[]) {
-    const weaponList = ['突＆銃', '打＆弓', '斬＆銃', '突＆弓', '打＆魔', '斬＆弓', '突＆魔', '打＆銃', '斬＆魔']
-    const date = this.GetStartDate(targetYear, targetMonth)
-    let index = this.GetFirstWeaponIndex(date)
-    let row: object[] = []
-    const week: object[] = []
-
-    const targetEventList = this.GetTargetEventList(targetYear, targetMonth, eventList)
-
-    while (true) {
-      const isToday =
-        this.today.getFullYear() === date.getFullYear() &&
-        this.today.getMonth() === date.getMonth() &&
-        this.today.getDate() === date.getDate()
-
-      const isTargetMonth = date.getMonth() + 1 === targetMonth
-      const isRareMedal = this.IsActiveEvent(date, EventType.RareMedal, targetEventList)
-      const isMaterial = this.IsActiveEvent(date, EventType.Material, targetEventList)
-      const isRaid = this.IsActiveEvent(date, EventType.Raid, targetEventList)
-      const isRoar = this.IsActiveEvent(date, EventType.Roar, targetEventList)
-      const isAdvent = this.IsActiveEvent(date, EventType.Advent, targetEventList)
-      const isMagicalBook = this.IsActiveEvent(date, EventType.MagicalBook, targetEventList)
-
-      row.push({
-        date: date.getDate(),
-        day: date.getDay(),
-        weapon: weaponList[index],
-        isRareMedal,
-        isMaterial,
-        isRaid,
-        isRoar,
-        isAdvent,
-        isToday,
-        isTargetMonth,
-        isMagicalBook
-      })
-      date.setDate(date.getDate() + 1)
-      index = this.GetNextWeaponIndex(index)
-
-      if (date.getDay() === 0) {
-        week.push({ week: row })
-        row = []
-
-        if (date.getMonth() + 1 !== targetMonth) break
+  },
+  methods: {
+    DisplayNextMonth: function() {
+      if (this.month === 12) {
+        this.month = 1
+        this.year += 1
+      } else {
+        this.month += 1
       }
+      this.weekList = this.CreateWeekList(this.year, this.month, this.eventList)
+    },
+    DisplayLastMonth: function() {
+      if (this.month === 1) {
+        this.month = 12
+        this.year -= 1
+      } else {
+        this.month -= 1
+      }
+      this.weekList = this.CreateWeekList(this.year, this.month, this.eventList)
+    },
+    CreateWeekList(targetYear: number, targetMonth: number, eventList?: Event[]) {
+      const weaponList = ['突＆銃', '打＆弓', '斬＆銃', '突＆弓', '打＆魔', '斬＆弓', '突＆魔', '打＆銃', '斬＆魔']
+      const date = this.GetStartDate(targetYear, targetMonth)
+      let index = this.GetFirstWeaponIndex(date)
+      let row: object[] = []
+      const week: object[] = []
+
+      const targetEventList = this.GetTargetEventList(targetYear, targetMonth, eventList)
+
+      while (true) {
+        const isToday =
+          this.today.getFullYear() === date.getFullYear() &&
+          this.today.getMonth() === date.getMonth() &&
+          this.today.getDate() === date.getDate()
+
+        const isTargetMonth = date.getMonth() + 1 === targetMonth
+        const isRareMedal = this.IsActiveEvent(date, EventType.RareMedal, targetEventList)
+        const isMaterial = this.IsActiveEvent(date, EventType.Material, targetEventList)
+        const isRaid = this.IsActiveEvent(date, EventType.Raid, targetEventList)
+        const isRoar = this.IsActiveEvent(date, EventType.Roar, targetEventList)
+        const isAdvent = this.IsActiveEvent(date, EventType.Advent, targetEventList)
+        const isMagicalBook = this.IsActiveEvent(date, EventType.MagicalBook, targetEventList)
+
+        row.push({
+          date: date.getDate(),
+          day: date.getDay(),
+          weapon: weaponList[index],
+          isRareMedal,
+          isMaterial,
+          isRaid,
+          isRoar,
+          isAdvent,
+          isToday,
+          isTargetMonth,
+          isMagicalBook
+        })
+        date.setDate(date.getDate() + 1)
+        index = this.GetNextWeaponIndex(index)
+
+        if (date.getDay() === 0) {
+          week.push({ week: row })
+          row = []
+
+          if (date.getMonth() + 1 !== targetMonth) break
+        }
+      }
+      return week
+    },
+    GetStartDate(year: number, month: number): Date {
+      const date: Date = new Date(year + '/' + month + '/' + '01')
+
+      // カレンダーの開始日が日曜日になるように、月の開始日から曜日を引く
+      date.setDate(date.getDate() - date.getDay())
+      return date
+    },
+    GetFirstWeaponIndex(date: Date): number {
+      const baseDate = new Date('2018/12/2')
+      const diff = date.getTime() - baseDate.getTime()
+      const diffDay = diff / (1000 * 60 * 60 * 24)
+      let index = diffDay % 9
+
+      // 差分がマイナスの場合はプラスになるように調整
+      if (index < 0) index += 9
+
+      return index
+    },
+    GetTargetEventList(year: number, month: number, eventList?: Event[]) {
+      if (eventList === undefined) return undefined
+
+      const targetMonth = year.toString() + month.toString().padStart(2, '0')
+      return eventList.filter(element => element.month === targetMonth)
+    },
+    GetNextWeaponIndex(index: number) {
+      if (index === 8) return 0
+
+      return index + 1
+    },
+    IsActiveEvent(date: Date, eventType: EventType, targetEventList?: Event[]) {
+      if (targetEventList === undefined) return false
+
+      const event = targetEventList.find(element => element.type === eventType)
+      if (event === undefined) return false
+
+      if (event.startDate <= date.getDate() && date.getDate() <= event.endDate) return true
+
+      return false
+    },
+    async GetEventList() {
+      const url = 'https://script.google.com/macros/s/AKfycbwdFst2WSgIFv70mkmVtaVsx4ygVgVJfXZeWaBvgt7wkD0hzaGQ/exec'
+      const response = await axios.get<Array<Event>>(url)
+      if (response.status !== 200) return
+
+      const eventList = await response.data
+      return eventList
     }
-    return week
   }
-
-  private GetStartDate(year: number, month: number): Date {
-    const date: Date = new Date(year + '/' + month + '/' + '01')
-
-    // カレンダーの開始日が日曜日になるように、月の開始日から曜日を引く
-    date.setDate(date.getDate() - date.getDay())
-    return date
-  }
-
-  private GetFirstWeaponIndex(date: Date): number {
-    const baseDate = new Date('2018/12/2')
-    const diff = date.getTime() - baseDate.getTime()
-    const diffDay = diff / (1000 * 60 * 60 * 24)
-    let index = diffDay % 9
-
-    // 差分がマイナスの場合はプラスになるように調整
-    if (index < 0) index += 9
-
-    return index
-  }
-
-  private GetTargetEventList(year: number, month: number, eventList?: Event[]) {
-    if (eventList === undefined) return undefined
-
-    const targetMonth = year.toString() + month.toString().padStart(2, '0')
-    return eventList.filter(element => element.month === targetMonth)
-  }
-
-  private GetNextWeaponIndex(index: number) {
-    if (index === 8) return 0
-
-    return index + 1
-  }
-
-  private IsActiveEvent(date: Date, eventType: EventType, targetEventList?: Event[]) {
-    if (targetEventList === undefined) return false
-
-    const event = targetEventList.find(element => element.type === eventType)
-    if (event === undefined) return false
-
-    if (event.startDate <= date.getDate() && date.getDate() <= event.endDate) return true
-
-    return false
-  }
-
-  private async GetEventList() {
-    const url = 'https://script.google.com/macros/s/AKfycbwdFst2WSgIFv70mkmVtaVsx4ygVgVJfXZeWaBvgt7wkD0hzaGQ/exec'
-    const response = await axios.get<Array<Event>>(url)
-    if (response.status !== 200) return
-
-    const eventList = await response.data
-    return eventList
-  }
-}
+})
 </script>
